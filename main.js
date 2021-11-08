d53382c86ebbeca4b8b48b631dc206bfee83b60c
Amar Khan <amar.khan@auto1.com>
2021-11-08 14:09:28 +0100

    param changes

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const checkProdFreeze = require("./lib/check-prod-freeze");
 const core = require("@actions/core");
 const forcedeploy = core.getInput("force_deploy");
 const env = core.getInput("env");
+const freezestart = core.getInput("freezeStart");
+const freezeend = core.getInput("freezeEnd");
 
 try {
-    checkProdFreeze("16:00","18:00",env,forcedeploy); 
+    checkProdFreeze(freezestart,freezeend,env,forcedeploy); 
 } catch (error) {
     core.setFailed(error.message);
 }
